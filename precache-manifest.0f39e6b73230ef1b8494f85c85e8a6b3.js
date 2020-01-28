de9c2d4d3fd882d11427368af53e31cd21c025eb
kyrillos <kyrillos417@gmail.com>
2020-01-27 20:55:41 -0800

    Updates

---
+++ precache-manifest.0f39e6b73230ef1b8494f85c85e8a6b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a4edcbbf27324e59aa6816fb3208a8",
+    "url": "/Budget-Calculator/index.html"
+  },
+  {
+    "revision": "0b58e42475e01be11573",
+    "url": "/Budget-Calculator/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "904b85802024e6a2a192",
+    "url": "/Budget-Calculator/static/css/main.ab22c2f1.chunk.css"
+  },
+  {
+    "revision": "0b58e42475e01be11573",
+    "url": "/Budget-Calculator/static/js/2.d70def70.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Budget-Calculator/static/js/2.d70def70.chunk.js.LICENSE"
+  },
+  {
+    "revision": "904b85802024e6a2a192",
+    "url": "/Budget-Calculator/static/js/main.40d3001b.chunk.js"
+  },
+  {
+    "revision": "d28ee7e534bf052293e4",
+    "url": "/Budget-Calculator/static/js/runtime-main.628c69ca.js"
+  }
+]);
